8b544e897976d72f66169b0f9b6c63f0fef5c837
SpragueSarah <SpragueSarah@users.noreply.github.com>
2016-03-20 15:28:20 -0700

    Added files via upload
    Capstone exploration file dataset and proposal

---
+++ CapstoneExploration.R
@@ -0,0 +1,6 @@
+# Exploring my billionaires dataset
+billionaires <- read.dta("/Users/ssprague/Documents/Fun/Springboard Data Science/Billionaire Characteristics Database/Billionaires1996-2014.dta")
+str(billionaires)
+
+billionaires_detail <- read.dta("/Users/ssprague/Documents/Fun/Springboard Data Science/Billionaire Characteristics Database/Billionaires-2001and2014detail.dta")
+str(billionaires_detail)
